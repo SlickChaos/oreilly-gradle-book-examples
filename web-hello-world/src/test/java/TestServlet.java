7d4e9b9ba067f226af8a8e34e78ea9553ac10062
Ray Reynolds <Ray.Reynolds@aucerna.com>
2020-04-10 15:14:13 +0100

    comment on extra UT

--- web-hello-world/src/test/java/TestServlet.java
+++ web-hello-world/src/test/java/TestServlet.java
@@ -24,6 +24,7 @@ public class TestServlet extends Mockito{
         assertTrue(sw.toString().contains("hello, world"));  
 	}
 	
+	// initial concept to make sure Unit Tests could be run
 	@Test
     public void testAlwaysPass() {
         assertTrue(true);

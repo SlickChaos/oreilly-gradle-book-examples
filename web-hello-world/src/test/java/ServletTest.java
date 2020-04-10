df8df54be91cf8c30047bf908f21a94a2b1951f7
Ray Reynolds <Ray.Reynolds@aucerna.com>
2020-04-10 14:39:31 +0100

    fiddling10

--- web-hello-world/src/test/java/ServletTest.java
+++ web-hello-world/src/test/java/ServletTest.java
@@ -1,53 +1,34 @@
+
 package org.gradle.examples.web;
- 
-import static org.junit.Assert.*;
-//import static org.mockito.Mockito.when;
-// 
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.io.StringWriter;
-// 
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
- 
-//import org.junit.Before;
+
+import static org.junit.Assert.assertTrue; 
+import static org.junit.Assert.fail;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.junit.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 
+import org.gradle.examples.web.Servlet;
 
-//import org.junit.Test;
-//import static org.junit.Assert.*;
+public class ServletTest extends Mockito{
+	@Test
+    public void testServlet() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class); 
+        StringWriter sw = new StringWriter();
+        PrintWriter writer = new PrintWriter(sw);
+        writer.print("hello, world");
+        
+        when(response.getWriter()).thenReturn(writer);
+        new Servlet().doGet(request, response);
+        assertTrue(sw.toString().contains("hello, world"));
+        
+	}
 
-public class Servlet {
- 
-//  @Mock
-//  HttpServletRequest request;
-//
-//  @Mock
-//  HttpServletResponse response;
-//
-//  @Before
-//  public void setUp() throws Exception {
-//	MockitoAnnotations.initMocks(this);
-//  }
-
-  @Test
-  public void testAlwaysPass() {
-	assertTrue(true);
-  }
-
-//s  @Test
-//s  public void testHelloWorldPrinting() throws IOException, ServletException {
-//s        StringWriter sw = new StringWriter();
-//s        PrintWriter pw = new PrintWriter(sw);
-//s        
-//s        when(response.getWriter()).thenReturn(pw);
-//s 
-//s        Servlet Servlet = new Servlet();
-//s        Servlet.doGet(request, response);
-//s        String result = sw.getBuffer().toString().trim();
-//s        assertEquals(result, new String("hello, world"));
-//s  }
+	
 }

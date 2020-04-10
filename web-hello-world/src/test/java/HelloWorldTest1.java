9ad5fe4738784168b4be2cce9265469fd2d8e912
Ray Reynolds <Ray.Reynolds@aucerna.com>
2020-04-10 12:38:07 +0100

    responses UT

--- web-hello-world/src/test/java/HelloWorldTest1.java
+++ web-hello-world/src/test/java/HelloWorldTest1.java
@@ -1,10 +1,53 @@
-import org.junit.Test;
+package org.gradle.examples.web;
+ 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-public class HelloWorldTest1 {
+
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+
+public class MyServletTest {
+ 
+  @Mock
+  HttpServletRequest request;
+
+  @Mock
+  HttpServletResponse response;
+
+  @Before
+  public void setUp() throws Exception {
+	MockitoAnnotations.initMocks(this);
+  }
+
   @Test
-  public void testHelloWorldPrinting1() {
-      Servlet.doGet(null);
-      assertTrue(true);
+  public void testHelloWorldPrinting() throws IOException, ServletException {
+        //when(request.getParameter("fn")).thenReturn("Vinod");
+        //when(request.getParameter("ln")).thenReturn("Kashyap");
+ 
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+         
+        when(response.getWriter()).thenReturn(pw);
+ 
+        MyServlet myServlet =new MyServlet();
+        myServlet.doGet(request, response);
+        String result = sw.getBuffer().toString().trim();
+        assertEquals(result, new String("hello, world"));
+
+	  
   }
 }
